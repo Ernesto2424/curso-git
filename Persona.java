7b91e3d1b19cc7f9bc09401ffd118126cfde0d85
Ernesto2424 <ernestofm2001@gmail.com>
2023-07-15 16:58:11 -0600

    se agrego una nueva linea al codigo

--- Persona.java
+++ Persona.java
@@ -3,6 +3,8 @@ public class Persona{
 public static void main(String[] args) {
  System.out.println("hola mundo");   
  System.out.println("que tal curso");
+ System.out.println("nuevo texto");    
+
 }
 
 }
